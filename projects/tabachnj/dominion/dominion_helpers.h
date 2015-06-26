26c917c23f3e489a4a93bf665501395ae3e811c7
Jeff Tabachnick <jtabachnick@gmail.com>
2015-06-25 20:46:21 -0400

    added play adventure, smithy, councilRoom, village, and baron. cleaned up formatting

--- projects/tabachnj/dominion/dominion_helpers.h
+++ projects/tabachnj/dominion/dominion_helpers.h
@@ -11,5 +11,9 @@ int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
-
+void playAdventurer(int temphand[], struct gameState *state, int currentPlayer);
+void playSmithy(int currentPlayer, struct gameState *state, int handPos);
+void playVillage(int currentPlayer, struct gameState *state, int handPos);
+void playBaron(int currentPlayer, struct gameState *state);
+void playCouncilRoom(int currentPlayer, struct gameState *state, int handPos);
 #endif
