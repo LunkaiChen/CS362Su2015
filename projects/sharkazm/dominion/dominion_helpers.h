0ba473a4700b23a120bad777eaf0a01c3a70cc3c
sharkazm <sharkazm@onid.oregonstate.edu>
2015-06-28 18:35:58 -0400

    adventurerCard() added

--- projects/sharkazm/dominion/dominion_helpers.h
+++ projects/sharkazm/dominion/dominion_helpers.h
@@ -12,5 +12,6 @@ int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
 void smithyCard(struct gameState *state, int currentPlayer, int handPos);
+void adventurerCard(struct gameState *state, int currentPlayer);
 
 #endif
