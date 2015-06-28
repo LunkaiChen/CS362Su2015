a3f99ed3cdca42e141917e6d1425794e1530b473
sharkazm <sharkazm@onid.oregonstate.edu>
2015-06-28 19:07:18 -0400

    villageCard() added

--- projects/sharkazm/dominion/dominion.c
+++ projects/sharkazm/dominion/dominion.c
@@ -698,6 +698,21 @@ void adventurerCard(struct gameState *state, int currentPlayer){
     }
 }
 
+//Village Card refactored into its own function:  
+
+void villageCard(struct gameState *state, int currentPlayer, int handPos){
+      
+      //+1 Card
+      drawCard(currentPlayer, state);
+      drawCard(currentPlayer, state);     //will draw twice instead of once
+      
+      //+2 Actions
+      state->numActions = handPos + 2;  
+      
+      //discard played card from hand
+      discardCard(handPos, currentPlayer, state, 0);
+}
+
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
   int i;
@@ -724,6 +739,10 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
     	
     case adventurer:
       adventurerCard(state, currentPlayer);
+      return 0;
+      
+    case village:
+      village_played(state, currentPlayer, handPos);
       return 0;
       
     case council_room:
