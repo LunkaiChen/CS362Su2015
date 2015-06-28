b807e0357d1ab25834631fef2b07e40892f442f5
Brett Fedack <fedackb@onid.oregonstate.edu>
2015-06-28 16:40:45 -0600

    Documented 'adventurer' card

--- projects/fedackb/dominion/documentation.c
+++ projects/fedackb/dominion/documentation.c
@@ -8,3 +8,17 @@ Smithy Card
 	player's hand by invoking the 'drawCard()' routine three times. The played
 	'smithy' card is then discarded from the player's hand by calling
 	'discardCard()'.
+
+Adventurer Card
+
+	The 'adventurer' is a kingdom card. Its value, as can be seen in the query
+	function 'getCost()' of the file 'dominion.c,' is hardcoded to six currency
+	units. Upon playing an 'adventurer' card, the 'cardEffect()' function is
+	invoked by 'playCard()', and relevant actions are executed that modify the
+	game's state. Specifically, playing it adds cards for the current player by
+	invoking 'drawCard()' until the count of 'drawntreasure' cards (initially
+	zero) reaches two. Treasure cards are enumerated as 'copper', 'silver', or
+	'gold', and remain in the current player's hand. Non-treasure cards are
+	stored in the temporary array 'temphand'. After drawing two treasure cards,
+	each card in the temporary hand is then discarded by directly modifying the
+	'discard' and 'discardCount' data members of the given game's state.
