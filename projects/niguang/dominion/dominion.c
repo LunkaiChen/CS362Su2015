c8c3bb742d46c5e74b55f1f9bdb4353e41169ab3
niguangting@gmail.com <nig@flip2.engr.oregonstate.edu>
2015-06-28 16:58:09 -0700

    Done, finish assignment2

--- projects/niguang/dominion/dominion.c
+++ projects/niguang/dominion/dominion.c
@@ -1358,28 +1358,5 @@ int updateCoins(int player, struct gameState *state, int bonus)
 
   return 0;
 }
-/*
-void adventurerCard(struct gameState *state, int currentPlayer, int cardDrawn, int temphand[MAX_HAND]){
-     int drawntreasure=0;
-     int z=0;// this is the counter for the temp hand
-     while(drawntreasure<2){
-        if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-          shuffle(currentPlayer, state);
-        }
-        drawCard(currentPlayer, state);
-        cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-        if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-          drawntreasure++;
-        else{
-          temphand[z]=cardDrawn;
-          state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-          z++;
-        }
-      }
-      while(z-1>=0){
-        state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-        z=z-1;
-      }
-}*/
 //end of dominion.c
 
