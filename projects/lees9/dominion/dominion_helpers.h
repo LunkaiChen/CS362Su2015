933447f8393130cc89dd52df1d8baf446fc7b039
lees9 <lees9@onid.oregonstate.edu>
2015-06-28 20:35:39 -0700

    updated card implementations in dominion.c and added headers to dominion_helpers.h

--- projects/lees9/dominion/dominion_helpers.h
+++ projects/lees9/dominion/dominion_helpers.h
@@ -5,11 +5,17 @@
 
 int drawCard(int player, struct gameState *state);
 int updateCoins(int player, struct gameState *state, int bonus);
-int discardCard(int handPos, int currentPlayer, struct gameState *state, 
+int discardCard(int handPos, int currentPlayer, struct gameState *state,
 		int trashFlag);
 int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
-int cardEffect(int card, int choice1, int choice2, int choice3, 
+int cardEffect(int card, int choice1, int choice2, int choice3,
 	       struct gameState *state, int handPos, int *bonus);
+
+void call_adventurer(int currentPlayer, struct gameState *state);
+void call_council_room(int currentPlayer, int handPos, struct gameState *state);
+void call_smithy(int currentPlayer, int handPos, struct gameState *state);
+void call_village(int currentPlayer, int handPos, struct gameState *state);
+void call_great_hall(int currentPlayer, int handPos, struct gameState *state);
 
 #endif
