7b9a9ee77f89d1a8c04697ea84b88d69798d5e5e
Jonathan Beaty <beatyj@onid.oregonstate.edu>
2015-06-28 23:13:03 -0700

    Added Dominion folder and all local assignment 2 fixes

---
+++ projects/beatyj/dominion/documentation.c
@@ -0,0 +1,57 @@
+/***************************************************\
+| Jonathan Beaty								    |
+| beatyj@onid.oregonstate.edu 						|
+| CS362 Summer 2015								    |
+| Assignment # 2 									|
+| documentation.c 									|
+| 													|
+| This file explains the adventurer and				|
+| smithy cards in dominion as well as the code		| 
+| and methods used for each respectively			|
+\***************************************************/
+
+/****************/
+Adventurer cards
+/****************/
+
+Draw cards until two treasure cards are drawn
+
+Track whether the drawn card is a treasure card: copper, silver or gold 
+Increment count of treasure cards based on if drwan card is a treasure card
+
+If the card is not a treasure card then it decrements the overall handcount for the player 
+and increases the count of cards that has been drawn.
+
+Functionally speaking, it keeps track of a temporary hand that each drawn card is added to be displayed separately.
+Nothing is actually done with the temporary hand once two treasures are drawn 
+so the cards are placed back into the player's discard pile hence state->discard.
+
+/****************/
+Smithy Cards
+/****************/
+
+Draws additional 3 cards, adding them to the current player's hand. 
+
+Once the cards are drawn via drawCard(),
+the discardCard() function is called and all the player's cards are discarded.
+
+/****************/
+Methods
+/****************/
+
+discardCard()
+
+The discardCard() function places the played card in the played card pile. 
+If the card is not trashed, add the card to the played card pile. Then Set played
+card to -1 and remove card from player's hand
+
+If the last card in the hand array is played the decrement number of cards in hand.
+Else if only one card in hand, decrement number of cards in hand.
+Else, replace discarded card with last card in hand, set last card to -1 and
+decrement number of cards in hand.
+
+updateCoins() 
+
+The updateCoins() function counts the number of coins a player has each turn by
+resetting the player's coins, adding the turns current coins for each treasure card 
+and adding in any bonuses
---
+++ projects/beatyj/dominion/refactor.c
@@ -0,0 +1,60 @@
+/***************************************************\
+| Jonathan Beaty								    |
+| beatyj@onid.oregonstate.edu 						|
+| CS362 Summer 2015								    |
+| Assignment # 2 									|
+| refactor.c								   	    |
+| 													|
+| This file tells which cards have been refactored	|
+| and what bugs have been introduced				|
+\***************************************************/
+
+All bugs are made inside the ref methods. Methods get called in cardEffect() for each case.
+
+Changed Cards:
+
+/***************/
+Adventurer card
+/***************/
+
+The refactored function is called adventurer_ref.
+
+Bugs:
+Drawn treasure is initially set to 1 instead of 0 so the count is off by 1
+and the player initally starts with 1 fake treasure
+
+/***************/
+Embargo Card
+/***************/
+
+The refactored function is called embargo_ref.
+
+Bugs:
++3 coins are added instead of +2 
+
+/***************/
+Feast Card
+/***************/
+
+The refactored function is called feast_ref.
+
+Bugs:
+Reset Hand shuld be i=0 so for loop is off by 1 for reset
+
+/***************/
+Salvager Card
+/***************/
+
+The refactored function is called salvager_ref.
+
+Bugs:
+If case should be for choice1 not choice2
+
+/***************/
+Smithy card 
+/***************/
+
+The refactored function is called smithy_ref.
+
+Bugs:
+Set i < 4 instead of 3 thus 4 cards are drawn instead of 3.
