41147844534126bc76ab9d42788478a541915ed7
Brett Fedack <fedackb@onid.oregonstate.edu>
2015-06-28 18:39:14 -0600

    Refactored minion card

--- projects/fedackb/dominion/dominion.c
+++ projects/fedackb/dominion/dominion.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 
 int adventurerCardEffect(struct gameState *state);
+int minionCardEffect(int handPos, int choice1, int choice2, struct gameState *state);
 int salvagerCardEffect(int handPos, int choice1, struct gameState *state);
 int smithyCardEffect(int handPos, struct gameState *state);
 int villageCardEffect(int handPos, struct gameState *state);
@@ -881,55 +882,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case minion:
-      //+1 action
-      state->numActions++;
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-			
-      if (choice1)		//+2 coins
-	{
-	  state->coins = state->coins + 2;
-	}
-			
-      else if (choice2)		//discard hand, redraw 4, other players with 5+ cards discard hand and draw 4
-	{
-	  //discard hand
-	  while(numHandCards(state) > 0)
-	    {
-	      discardCard(handPos, currentPlayer, state, 0);
-	    }
-				
-	  //draw 4
-	  for (i = 0; i < 4; i++)
-	    {
-	      drawCard(currentPlayer, state);
-	    }
-				
-	  //other players discard hand and redraw if hand size > 4
-	  for (i = 0; i < state->numPlayers; i++)
-	    {
-	      if (i != currentPlayer)
-		{
-		  if ( state->handCount[i] > 4 )
-		    {
-		      //discard hand
-		      while( state->handCount[i] > 0 )
-			{
-			  discardCard(handPos, i, state, 0);
-			}
-							
-		      //draw 4
-		      for (j = 0; j < 4; j++)
-			{
-			  drawCard(i, state);
-			}
-		    }
-		}
-	    }
-				
-	}
-      return 0;
+		return minionCardEffect(handPos, choice1, choice2, state);
 		
     case steward:
       if (choice1 == 1)
@@ -1308,6 +1261,63 @@ int adventurerCardEffect(struct gameState *state)
 	while(z-1>=0) {
 		state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
 		z=z-1;
+	}
+	return 0;
+}
+
+int minionCardEffect(int handPos, int choice1, int choice2, struct gameState *state)
+{
+	int i;
+	int j;
+	int currentPlayer = whoseTurn(state);
+
+	//+1 action
+	state->numActions++;
+
+	//discard card from hand
+	discardCard(handPos, currentPlayer, state, 0);
+
+	if (choice1) //+2 coins
+	{
+		state->coins = state->coins + 2;
+	}
+
+	else if (choice2) //discard hand, redraw 4, other players with 5+ cards discard hand and draw 4
+	{
+		//discard hand
+		while(numHandCards(state) > 0)
+		{
+			discardCard(handPos, currentPlayer, state, 0);
+		}
+
+		//draw 4
+		for (i = 0; i < 4; i++)
+		{
+			drawCard(currentPlayer, state);
+		}
+
+		//other players discard hand and redraw if hand size > 4
+		for (i = 0; i < state->numPlayers; i++)
+		{
+			if (i != currentPlayer)
+			{
+				if ( state->handCount[i] > 4 )
+				{
+					//discard hand
+					while( state->handCount[i] > 0 )
+					{
+						discardCard(handPos, i, state, 0);
+					}
+
+					//draw 4
+					for (j = 0; j < 4; j++)
+					{
+						drawCard(i, state);
+					}
+				}
+			}
+		}
+
 	}
 	return 0;
 }
